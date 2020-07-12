8c4a6809e91c9389448bf1b0b88edfeb97c8f159
yusuf-pathan-vst-au4 <yusufp04@gmail.com>
2020-07-12 23:16:56 +0530

    added APIs

--- Controllers/following.js
+++ Controllers/following.js
@@ -1,12 +1,12 @@
-const Following = require('../Models/Following');
-const express = require('express');
+const Following = require("../Models/Following");
+const express = require("express");
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     let following = await Following.findAll();
     return res.status(200).json({
-      message: 'success',
+      message: "success",
       users: following,
     });
   } catch (error) {
@@ -15,4 +15,60 @@ router.get('/', async (req, res) => {
   }
 });
 
-module.exports = router
+router.post("/", async (req, res) => {
+  const { userid, following } = req.body;
+  if (!userid || !following) {
+    return res.status(400);
+  }
+  try {
+    let following = await Following.create({
+      userid,
+      following,
+    });
+    console.log(following);
+    res.status(201).json({
+      message: "success",
+      following,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400);
+  }
+});
+
+router.put("/", async (req, res) => {
+  try {
+    const id = req.body.id;
+    console.log(id);
+    let following = await Following.update(
+      {
+        userid,
+        following,
+      },
+      {
+        where: { id: id },
+      }
+    );
+    console.log(following);
+    res.send(following);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.delete("/", async (req, res) => {
+  try {
+    const id = req.body.id;
+    console.log("Body >> ", body);
+    const following = await Following.destroy({
+      where: {
+        id: id,
+      },
+    });
+    res.json({ following });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
--- Controllers/users.js
+++ Controllers/users.js
@@ -15,4 +15,64 @@ router.get("/", async (req, res) => {
   }
 });
 
-module.exports = router
+router.post("/", async (req, res) => {
+  const { username, password, name, email } = req.body;
+  if (!username || !password || !name || !email) {
+    return res.status(400);
+  }
+  try {
+    let user = await User.create({
+      username,
+      password,
+      name,
+      email,
+    });
+    console.log(user);
+    res.status(201).json({
+      message: "success",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400);
+  }
+});
+
+router.put("/", async (req, res) => {
+  try {
+    const id = req.body.id;
+    console.log(id);
+    let user = await User.update(
+      {
+        username,
+        password,
+        email,
+        name,
+      },
+      {
+        where: { id: id },
+      }
+    );
+    console.log(user);
+    res.send(user);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.delete("/", async (req, res) => {
+  try {
+    const id = req.body.id;
+    console.log("Body >> ", body);
+    const user = await User.destroy({
+      where: {
+        id: id,
+      },
+    });
+    res.json({ user });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
